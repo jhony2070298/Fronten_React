9bbb42d0566a7c04764177384b4018de8d6706bf
Davand043 <davidperez300516@correo.itm.edu.co>
2021-10-19 19:08:14 -0500

    se ajustan nombre de archivos en pages

--- src/routes/App.js
+++ src/routes/App.js
@@ -3,12 +3,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navegacion } from '../layouts/Navegacion';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Container, Image } from 'react-bootstrap';
-import { Register } from '../pages/Register';
-import { Login } from '../pages/Login';
-import { Formulario } from '../pages/Formulario';
-import { Usuario } from '../pages/Usuario';
-import { Administrador } from '../pages/Administrador';
-import { Editar } from '../pages/Editar';
+import { Register } from '../pages/register';
+import { Login } from '../pages/login';
+import { Formulario } from '../pages/formulario';
+import { Usuario } from '../pages/usuario';
+import { Administrador } from '../pages/administrador';
+import { Editar } from '../pages/editar';
 import { Inicio } from '../pages/Inicio';
 import LogoIH from '../img/logo_ih.jpg'
 import linguaskill from '../img/Linguaskill_from_480.jpeg'
